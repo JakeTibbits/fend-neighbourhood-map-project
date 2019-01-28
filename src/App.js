d425f21b198dd38480fbda01073e1e43cdd32ad9
Jake-Tibbits <jake@bonusmedia.co.uk>
2019-01-28 16:41:14 +0000

    import GoogleApiWrapper from google-maps-react and add wrapper to App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { GoogleApiWrapper } from 'google-maps-react'
+import { MapContainer } from './Map'
+import './App.css'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <MapContainer google={this.props.google}/>
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBUy5nSuV8dwh44xoOh7oSZZQP3NTPgS7E'
+})(App);
