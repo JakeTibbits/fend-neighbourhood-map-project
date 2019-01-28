b6b6ea33c88b1f352c2e9fd8b003649c7180d7b2
Jake-Tibbits <jake@bonusmedia.co.uk>
2019-01-28 18:53:24 +0000

    create placeData.JSON with 5 initial marker loactions and import

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react'
 import { GoogleApiWrapper } from 'google-maps-react'
 import MapContainer from './MapDisplay'
 import './App.css'
+import places from './placeData'
 
 
 class App extends Component {
+
+  state = {
+    places: places
+  }
+
   render() {
     return (
       <div className="App">
-        <MapContainer google={this.props.google} />
+        <MapContainer google={this.props.google} places={this.state.places}/>
       </div>
     )
   }
