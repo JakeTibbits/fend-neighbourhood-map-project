7f3d72f846fc5a2eac9e08c61e32628c25451df5
Jake-Tibbits <jake@bonusmedia.co.uk>
2019-01-30 14:43:29 +0000

    separate filter into new component

---
+++ src/PlaceFilter.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class PlaceFilter extends Component {
+
+  handleFilterChange = (e) => {
+    this.props.onChangeFilter(e.target.value)
+  }
+
+  render(){
+    const { filter, tags } = this.props
+    return (
+        <form className="placeFilter">
+          <label htmlFor="filterPlaces" className="sr-only">Filter places by tag:</label>
+          <select name="filterPlaces" value={(filter ? filter : "none")} onChange={this.handleFilterChange}>
+            <option value="none">Filter Places</option>
+            {
+              tags && (
+                tags.map((tag) => (
+                  <option key={tag} value={tag}>{tag}</option>
+                ))
+              )
+            }
+          </select>
+        </form>
+
+    )
+  }
+}
+
+export default PlaceFilter
